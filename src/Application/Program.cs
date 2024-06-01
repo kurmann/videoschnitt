c0bdbd0be9ba54f56adfeb96ae79cd45b461a557
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-06-01 10:30:09 +0200

    feat: Add initial Razor pages and layout components

--- src/Application/Program.cs
+++ src/Application/Program.cs
@@ -1,27 +1,33 @@
-﻿using Kurmann.Videoschnitt.Engine;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace Kurmann.Videoschnitt.Application;
 
-internal class Program
+public class Program
 {
-    static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
-
-    public static IHostBuilder CreateHostBuilder(string[] args)
+    public static void Main(string[] args)
     {
-        return Host.CreateDefaultBuilder(args)
-            .ConfigureAppConfiguration((hostingContext, config) =>
-            {
-                if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == Environments.Development)
-                {
-                    config.AddUserSecrets<Program>();
-                }
-            })
-            .ConfigureServices((hostContext, services) =>
-            {
-                services.AddEngine(hostContext.Configuration);
-            });
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+
+        var app = builder.Build();
+
+        if (!app.Environment.IsDevelopment())
+        {
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.MapBlazorHub();
+        app.MapFallbackToPage("/_Host");
+
+        app.Run();
     }
 }
