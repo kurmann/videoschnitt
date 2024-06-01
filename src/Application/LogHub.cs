5dcfbd1e88820351e67e1627dcb6c5bc68c00ffd
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-06-01 14:57:24 +0200

    feat: Add LogHub SignalR Hub for real-time communication

---
+++ src/Application/LogHub.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace Kurmann.Videoschnitt.Application;
+public class LogHub : Hub
+{
+    // Hier kannst du serverseitige Methoden definieren, die vom Client aufgerufen werden können
+}
