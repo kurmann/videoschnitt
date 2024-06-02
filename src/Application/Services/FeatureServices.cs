c509cf455fa01ecd26ec1b228956bcc1ba2b8446
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-06-02 09:27:08 +0200

    feat: Refactor FeatureService to improve metadata processing

--- src/Application/Services/FeatureServices.cs
+++ src/Application/Services/FeatureServices.cs
@@ -6,22 +6,24 @@ namespace Kurmann.Videoschnitt.Application;
 /// <summary>
 /// Verantwortlich für die Verwaltung von Features
 /// </summary>
-public class FeatureService(MetadataProcessingService metadataProcessingService, IHubContext<LogHub> hubContext)
+public class FeatureService
 {
-    private readonly MetadataProcessingService _metadataProcessingService = metadataProcessingService;
-    private readonly IHubContext<LogHub> _hubContext = hubContext;
+    private readonly MetadataProcessingService _metadataProcessingService;
+    private readonly IHubContext<LogHub> _hubContext;
 
-    /// <summary>
-    /// Startet die Verarbeitung von Metadaten
-    /// </summary>
-    public async Task ProcessMetadataAsync()
+    public FeatureService(MetadataProcessingService metadataProcessingService, IHubContext<LogHub> hubContext)
     {
+        _metadataProcessingService = metadataProcessingService;
+        _hubContext = hubContext;
+
         _metadataProcessingService.MetadataProcessingEvent += async (sender, args) =>
         {
             await _hubContext.Clients.All.SendAsync("ReceiveLogMessage", args.Message);
         };
-
-        await _metadataProcessingService.ProcessMetadataAsync();
     }
 
+    public async Task ProcessMetadataAsync()
+    {
+        await _metadataProcessingService.ProcessMetadataAsync();
+    }
 }
