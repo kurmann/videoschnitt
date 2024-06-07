dcb751ee040c8cf60d92b80431aa91c09861e5de
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-06-07 07:03:33 +0200

    feat: Add ProcessMetadataRequestHandler class

---
+++ src/Application/Handlers/ProcessMetadataRequestHandler.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.SignalR;
+using Kurmann.Videoschnitt.Features.MetadataProcessor.Events;
+
+namespace Kurmann.Videoschnitt.Application;
+
+public class ProcessMetadataRequestHandler
+{
+    private readonly LogHub _logHub;
+
+    public ProcessMetadataRequestHandler(LogHub logHub)
+    {
+        _logHub = logHub;
+    }
+
+    public async Task Handle(ProcessMetadataRequest message)
+    {
+        var logMessage = "Anfrage zur Verarbeitung der Metadaten erhalten.";
+        await _logHub.SendMessage(logMessage);
+        Console.WriteLine(logMessage);
+    }
+}
