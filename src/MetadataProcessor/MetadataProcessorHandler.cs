cccb8997dc3a40c87442b4a55a291d4986bb45e2
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-06-11 09:55:31 +0000

    Move metadata process handler to feature root

--- src/MetadataProcessor/MetadataProcessorHandler.cs
+++ src/MetadataProcessor/MetadataProcessorHandler.cs
@@ -2,7 +2,7 @@ using Kurmann.Videoschnitt.Messages.Metadata;
 
 namespace Kurmann.Videoschnitt.MetadataProcessor.Handler;
 
-public class ProcessMetadataHandler(Engine engine)
+public class MetadataProcessorHandler(Engine engine)
 {
     private readonly Engine _engine = engine;
 
