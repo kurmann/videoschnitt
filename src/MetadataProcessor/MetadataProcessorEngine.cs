109cfdfcf78f6c336138388d87ce49e5b9318e70
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-06-11 09:57:04 +0000

    Removed unused usings

--- src/MetadataProcessor/MetadataProcessorEngine.cs
+++ src/MetadataProcessor/MetadataProcessorEngine.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Kurmann.Videoschnitt.MetadataProcessor.Services;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
