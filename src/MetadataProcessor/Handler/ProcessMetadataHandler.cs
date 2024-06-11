f8624855fb6fa4ada92d7f8cf1a3cc475cad9483
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-06-11 09:52:11 +0000

    Renamed to ProcessMetadataHandler

--- src/MetadataProcessor/Handler/ProcessMetadataHandler.cs
+++ src/MetadataProcessor/Handler/ProcessMetadataHandler.cs
@@ -2,7 +2,7 @@ using Kurmann.Videoschnitt.Messages.Metadata;
 
 namespace Kurmann.Videoschnitt.MetadataProcessor.Handler;
 
-public class ProcessMetadataRequestHandler(Engine engine)
+public class ProcessMetadataHandler(Engine engine)
 {
     private readonly Engine _engine = engine;
 
