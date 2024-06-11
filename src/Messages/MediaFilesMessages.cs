1afdf0e1d1a64b47baf077765761411001197b90
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-06-11 09:01:59 +0000

    feat: Add error event for unsupported media files during metadata processing

--- src/Messages/MediaFilesMessages.cs
+++ src/Messages/MediaFilesMessages.cs
@@ -2,3 +2,6 @@ namespace Kurmann.Videoschnitt.Messages.MediaFiles;
 
 // Event um eine Liste von unterstützten Medien-Dateien zu erhalten, die für die Metadaten-Verarbeitung verwendet werden können
 public record SupportedMediaFilesForMetadataProcessingFoundEvent(List<FileInfo> MediaFiles);
+
+// Event um eine Fehlermeldung zu erhalten, wenn die Liste von unterstützten Medien-Dateien nicht abgerufen werden konnte
+public record SupportedMediaFilesForMetadataProcessingFoundErrorEvent(string Error);
