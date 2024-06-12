69ede0c609da95590b8bd6dbe09ada5e7aa8f804
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-06-12 21:58:24 +0000

    Final Cut Pro Workflow without Wolverine

--- src/MetadataProcessor/MetadataProcessorEngine.cs
+++ src/MetadataProcessor/MetadataProcessorEngine.cs
@@ -1,9 +1,8 @@
 using Kurmann.Videoschnitt.MetadataProcessor.Services;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Wolverine;
-using Kurmann.Videoschnitt.Messages.MediaFiles;
 using Kurmann.Videoschnitt.MetadataProcessor.Entities;
+using CSharpFunctionalExtensions;
 
 namespace Kurmann.Videoschnitt.MetadataProcessor
 {
@@ -13,38 +12,37 @@ namespace Kurmann.Videoschnitt.MetadataProcessor
     public class MetadataProcessorEngine
     {
         private readonly MetadataProcessorSettings _settings;
-        private readonly ILogger<MetadataProcessorEngine> _logger;
-        private readonly IMessageBus _bus;
         private readonly MediaFileListenerService _mediaFileListenerService;
         private readonly MetadataProcessingService _metadataProcessingService;
 
-        public MetadataProcessorEngine(IOptions<MetadataProcessorSettings> settings,
-                                         ILogger<MetadataProcessorEngine> logger,
-                                         IMessageBus bus,
-                                         MediaFileListenerService mediaFileListenerService,
-                                         MetadataProcessingService metadataProcessingService)
+        public MetadataProcessorEngine(IOptions<MetadataProcessorSettings> settings, ILogger<MetadataProcessorEngine> logger, IMessageBus bus,
+            MediaFileListenerService mediaFileListenerService, MetadataProcessingService metadataProcessingService)
         {
             _settings = settings.Value;
-            _logger = logger;
-            _bus = bus;
             _mediaFileListenerService = mediaFileListenerService;
             _metadataProcessingService = metadataProcessingService;
         }
 
-        public async Task StartAsync(Action<WorkflowStatusUpdate>? statusCallback = null)
+        public async Task<Result> StartAsync(IProgress<string> progress)
         {
-            _logger.LogInformation("Steuereinheit für die Metadaten-Verarbeitung gestartet.");
+            progress.Report("Steuereinheit für die Metadaten-Verarbeitung gestartet.");
 
             // Liste alle unterstützten Medien-Dateien im Verzeichnis auf
             var mediaFiles = _mediaFileListenerService.GetSupportedMediaFiles();
             if (mediaFiles.IsFailure)
             {
-                await _bus.PublishAsync(new MediaFilesForMetadataProcessingFoundErrorEvent(mediaFiles.Error));
-                return;
+                progress.Report("Fehler beim Ermitteln der Medien-Dateien.");
+                return Result.Failure(mediaFiles.Error);
             }
 
             // Informiere über die Anzahl der gefundenen Medien-Dateien
-            await _bus.PublishAsync(new MediaFilesForMetadataProcessingFoundEvent(mediaFiles.Value));
+            progress.Report($"Anzahl der gefundenen Medien-Dateien: {mediaFiles.Value.Count}");
+
+            // Liste alle Medien-Dateien auf
+            foreach (var mediaFile in mediaFiles.Value)
+            {
+                progress.Report(mediaFile.Name);
+            }
 
             // Gruppiere die gefundenen Medien-Dateien nach Medienset. Ein Medienset ist eine Gruppe von Medien-Dateien, die zusammengehören.
             var mediaSets = MediasetCollection.Create(mediaFiles.Value,
@@ -54,22 +52,19 @@ namespace Kurmann.Videoschnitt.MetadataProcessor
                                                       _settings.FileTypeSettings.SupportedImageExtensions);
 
             // Informiere über den Beginn der Metadaten-Verarbeitung
-            await _bus.PublishAsync(new MediaFilesMetadataProcessingStartedEvent());
+            progress.Report("Beginne mit der Verarbeitung der Metadaten.");
 
             // Verarbeite die Metadaten der Medien-Dateien.
             var processedMediaFiles = await _metadataProcessingService.ProcessMetadataAsync(mediaFiles.Value);
             if (processedMediaFiles.IsFailure)
             {
-                await _bus.PublishAsync(new MediaFilesMetadataProcessingErrorEvent(processedMediaFiles.Error));
-                return;
+                progress.Report("Fehler bei der Verarbeitung der Metadaten.");
+                return Result.Failure(processedMediaFiles.Error);
             }
 
             // Informiere über die erfolgreiche Verarbeitung der Metadaten
-            await _bus.PublishAsync(new MediaFilesMetadataProcessingSuccessEvent(processedMediaFiles.Value));
-
-            _logger.LogInformation("Steuerung für die Metadaten-Verarbeitung abgeschlossen.");
+            progress.Report("Metadaten erfolgreich verarbeitet.");
+            return Result.Success();
         }
     }
 }
-
-public record WorkflowStatusUpdate(string Message, int Progress, bool IsError = false);
