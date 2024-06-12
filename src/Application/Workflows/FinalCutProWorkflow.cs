8ea61868fe962f31bf58598b862f0914023a3f45
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-06-12 12:24:04 +0000

    feat: Update FinalCutProWorkflow to include logging and metadata processing

--- src/Application/Workflows/FinalCutProWorkflow.cs
+++ src/Application/Workflows/FinalCutProWorkflow.cs
@@ -1,11 +1,17 @@
 using CSharpFunctionalExtensions;
+using Kurmann.Videoschnitt.MetadataProcessor;
 
 namespace Kurmann.Videoschnitt.Application.Workflows;
 
-public class FinalCutProWorkflow : IAsyncWorkflow
+public class FinalCutProWorkflow(ILogger<FinalCutProWorkflow> logger, MetadataProcessorEngine engine) : IAsyncWorkflow
 {
-    public Task<Result> ExecuteAsync()
+    public async Task<Result> ExecuteAsync()
     {
-        throw new NotImplementedException();
+        logger.LogInformation("Final Cut Pro Workflow gestartet.");
+
+        await engine.StartAsync();
+
+        logger.LogInformation("Final Cut Pro Workflow beendet.");
+        return Result.Success();
     }
 }
