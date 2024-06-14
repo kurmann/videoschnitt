eb3b20a7537a3b63f222bb3e49d627f9d65123e6
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-06-14 07:16:58 +0000

    Save xml file from quicktime and mpeg4

--- src/MetadataProcessor/MetadataProcessorEngine.cs
+++ src/MetadataProcessor/MetadataProcessorEngine.cs
@@ -70,81 +70,98 @@ namespace Kurmann.Videoschnitt.MetadataProcessor
                 // Informiere über den ermittelten Medientyp
                 progress.Report($"Medientyp für Datei {mediaFile.Name}: {mediaTypeResult.Value.GetType().Name}");
 
-                // Wenn die Datei ein Mpeg4-Video ist, ermittle die QuickTime-Movie-Variante
-                var infuseXml = Maybe<XDocument>.None;
-                if (mediaTypeResult.Value is Mpeg4Video mpeg4Video)
+                // Wenn die Datei ein QuickTime-Movie ist, extrahiere die Metadaten und erstelle ein Infuse-XML-Objekt
+                if (mediaTypeResult.Value is QuickTimeMovie quickTimeMovie)
                 {
-                    var quickTimeMovieVariantResult = _mediaSetVariantService.GetQuickTimeMovieVariant(mpeg4Video, mediaFiles.Value);
-                    if (quickTimeMovieVariantResult.IsFailure)
-                    {
-                        progress.Report($"Fehler beim Ermitteln der QuickTime-Movie-Variante für Mpeg4-Video {mpeg4Video.FileInfo.Name}: {quickTimeMovieVariantResult.Error}");
-                        progress.Report($"Extrahiere Metadaten aus Mpeg4-Video {mpeg4Video.FileInfo.Name}");
-
-                        var readMpeg4MetadataResult = await _infuseXmlService.ReadMetadataFromMpeg4Video(mpeg4Video, progress);
-                        if (readMpeg4MetadataResult.IsFailure)
-                        {
-                            progress.Report(readMpeg4MetadataResult.Error);
-                        }
-
-                        // Informiere über die extrahierten Metadaten im Infuse-XML-Format
-                        progress.Report($"Infuse-XML aus Metadaten von Mpeg4-Video {mpeg4Video.FileInfo.Name}: {readMpeg4MetadataResult.Value}");
-
-                        // Speichere das Infuse-XML-Objekt
-                        infuseXml = readMpeg4MetadataResult.Value;
-
-                        // Fahre mit der nächsten Datei fort
-                        continue;
-                    }
-
-                    // Prüfe ob eine QuickTime-Movie-Variante gefunden wurde
-                    if (quickTimeMovieVariantResult.Value.HasNoValue)
-                    {
-                        var readMpeg4MetadataResult = await _infuseXmlService.ReadMetadataFromMpeg4Video(mpeg4Video, progress);
-                        if (readMpeg4MetadataResult.IsFailure)
-                        {
-                            progress.Report(readMpeg4MetadataResult.Error);
-                        }
-
-                        // Informiere über die extrahierten Metadaten im Infuse-XML-Format
-                        progress.Report($"Infuse-XML aus Metadaten von Mpeg4-Video {mpeg4Video.FileInfo.Name}: {readMpeg4MetadataResult.Value}");
-
-                        // Speichere das Infuse-XML-Objekt
-                        infuseXml = readMpeg4MetadataResult.Value;
-
-                        // Fahre mit der nächsten Datei fort
-                        continue;
-                    }
-
-                    // Informiere über die gefundene QuickTime-Movie-Variante
-                    progress.Report($"QuickTime-Movie-Variante für Mpeg4-Video {mpeg4Video.FileInfo.Name}: {quickTimeMovieVariantResult.Value.Value.FileInfo.Name}");
-
-                    // Extrahiere Metadaten aus der QuickTime-Movie-Variante
-                    var readQuickTimeMetadataResult = await _infuseXmlService.ReadMetdataFromQuickTimeMovie(quickTimeMovieVariantResult.Value.Value, progress);
+                    var readQuickTimeMetadataResult = await _infuseXmlService.ReadMetdataFromQuickTimeMovie(quickTimeMovie, progress);
                     if (readQuickTimeMetadataResult.IsFailure)
                     {
                         progress.Report(readQuickTimeMetadataResult.Error);
                     }
 
                     // Informiere über die extrahierten Metadaten im Infuse-XML-Format
-                    progress.Report($"Infuse-XML aus Metadaten von QuickTime-Movie {quickTimeMovieVariantResult.Value.Value.FileInfo.Name}: {readQuickTimeMetadataResult.Value}");
+                    progress.Report($"Infuse-XML aus Metadaten von QuickTime-Movie {quickTimeMovie.FileInfo.Name}: {readQuickTimeMetadataResult.Value}");
 
                     // Speichere das Infuse-XML-Objekt
-                    infuseXml = readQuickTimeMetadataResult.Value;
+                    var infuseXml = readQuickTimeMetadataResult.Value;
+
+                    // Erstelle den Dateinamen für das Infuse-XML-Objekt
+                    var infuseXmlFileName = _mediaSetVariantService.GetInfuseXmlFileName(quickTimeMovie.FileInfo);
+                    if (infuseXmlFileName.IsFailure)
+                    {
+                        progress.Report(infuseXmlFileName.Error);
+                        
+                        // Fahre mit der nächsten Datei fort
+                        continue;
+                    }
+
+                    // Informiere über den Dateinamen des Infuse-XML-Objekts
+                    progress.Report($"Dateiname des Infuse-XML-Objekts für Medien-Objekt {mediaFile.FullName}: {infuseXmlFileName.Value.FullName}");
+
+                    // Schreibe die Datei
+                    infuseXml.Save(infuseXmlFileName.Value.FullName);
+
+                    // Informiere über den Erfolg
+                    progress.Report($"Infuse-XML-Datei für Medien-Objekt {mediaFile.FullName} erfolgreich geschrieben.");
 
                     // Fahre mit der nächsten Datei fort
                     continue;
                 }
 
-                // Ermittle den Dateinamen des Infuse-XML-Objekts. Der Dateiname entspricht dem Dateinamen des Medien-Objekts ohne Varianten-Suffix und mit der Dateiendung '.xml'
-                var infuseXmlFileName = _mediaSetVariantService.GetInfuseXmlFileName(mediaFile);
-                if (infuseXmlFileName.IsFailure)
-                {
-                    progress.Report(infuseXmlFileName.Error);
-                    continue;
-                }
 
-                // Informiere über den Dateinamen des Infuse-XML-Objekts
-                progress.Report($"Dateiname des Infuse-XML-Objekts für Medien-Objekt {mediaFile.FullName}: {infuseXmlFileName.Value.FullName}");
+                // Wenn die Datei ein Mpeg4-Video ist, dann schreibe die Infuse-XML-Datei nur wenn kein Infuse-XML-Objekt bereits besteht, 
+                // da die QuickTime-Movie-Variante bevorzugt wird aufgrund der besseren Metadaten
+                if (mediaTypeResult.Value is Mpeg4Video mpeg4Video)
+                {
+                    // Informiere über das Mpeg4-Video und was wir nun vorhaben
+                    progress.Report("Mpeg4-Video gefunden. Es wird geprüft, ob bereits ein Infuse-XML-Objekt existiert.");
+
+                    // Ermittle den Dateinamen des Infuse-XML-Objekts, nachdem wir suchen, ob bereits ein Infuse-XML-Objekt existiert
+                    var infuseXmlFileName = _mediaSetVariantService.GetInfuseXmlFileName(mpeg4Video.FileInfo);
+                    if (infuseXmlFileName.IsFailure)
+                    {
+                        progress.Report(infuseXmlFileName.Error);
+
+                        // Fahre mit der nächsten Datei fort
+                        continue;
+                    }
+
+                    // Informiere über den Dateinamen des Infuse-XML-Objekts
+                    progress.Report($"Dateiname des Infuse-XML-Objekts für Medien-Objekt {mediaFile.FullName}: {infuseXmlFileName.Value.FullName}");
+
+                    // Prüfe ob bereits ein Infuse-XML-Objekt existiert
+                    if (infuseXmlFileName.Value.Exists)
+                    {
+                        progress.Report($"Infuse-XML-Datei für Medien-Objekt {mediaFile.FullName} existiert bereits. Es wird keine neue Datei geschrieben.");
+
+                        // Fahre mit der nächsten Datei fort
+                        continue;
+                    }
+
+                    // Informiere über das Fehlen des Infuse-XML-Objekts und was wir nun vorhaben
+                    progress.Report($"Infuse-XML-Datei für Medien-Objekt {mediaFile.FullName} existiert nicht. Es wird eine neue Datei geschrieben.");
+
+                    // Extrahiere die Metadaten und erstelle ein Infuse-XML-Objekt
+                    progress.Report($"Extrahiere Metadaten aus Mpeg4-Video {mpeg4Video.FileInfo.Name}");
+                    var readMpeg4MetadataResult = await _infuseXmlService.ReadMetadataFromMpeg4Video(mpeg4Video, progress);
+                    if (readMpeg4MetadataResult.IsFailure)
+                    {
+                        progress.Report(readMpeg4MetadataResult.Error);
+                        continue;
+                    }
+
+                    // Informiere über die extrahierten Metadaten im Infuse-XML-Format
+                    progress.Report($"Infuse-XML aus Metadaten von Mpeg4-Video {mpeg4Video.FileInfo.Name}: {readMpeg4MetadataResult.Value}");
+
+                    // Speichere das Infuse-XML-Objekt
+                    var infuseXml = readMpeg4MetadataResult.Value;
+
+                    // Schreibe die Datei
+                    infuseXml.Save(infuseXmlFileName.Value.FullName);
+
+                    // Informiere über den Erfolg
+                    progress.Report($"Infuse-XML-Datei für Medien-Objekt {mediaFile.FullName} erfolgreich geschrieben.");
+                }
             }
 
             return Result.Success();
