0f6cb4e91a7a4052b55e1771ab8df9a090dedbb4
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-06-15 17:52:34 +0000

    Removed obsolete Messages project

--- src/Messages/HealthCheckMessages.cs
+++
@@ -1,7 +0,0 @@
-namespace Kurmann.Videoschnitt.Messages.HealthCheck;
-
-public record HealthCheckRequest();
-
-public record HealthCheckResponse(string FFmpegVersionInfo);
-
-public record HealthCheckFailedResponse(string ErrorMessage);
