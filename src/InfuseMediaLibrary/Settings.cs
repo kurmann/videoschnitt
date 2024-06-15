79fc7b2b298383f67f31e7594b3550c446dba42d
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-06-15 05:09:55 +0000

    settings code documentation

--- src/InfuseMediaLibrary/Settings.cs
+++ src/InfuseMediaLibrary/Settings.cs
@@ -4,5 +4,13 @@ public class Settings
 {
     public const string SectionName = "InfuseMediaLibrary";
 
+    /// <summary>
+    /// Wurzelverzeichnis der Infuse-Mediathek.
+    /// </summary>
     public string? LibraryPath { get; set; }
+
+    /// <summary>
+    /// Verzeichnis, in dem die Quelldateien liegen, die in die Infuse-Mediathek importiert werden sollen.
+    /// </summary>
+    public string? SourceDirectoryPath { get; set; }
 }
