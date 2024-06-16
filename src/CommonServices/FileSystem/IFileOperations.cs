58c8ee31193f26eecd3b4683041a783e46f363cd
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-06-16 17:59:15 +0000

    Defined file operations interface inlcuding inheritPermissions

--- src/CommonServices/FileSystem/IFileOperations.cs
+++ src/CommonServices/FileSystem/IFileOperations.cs
@@ -4,8 +4,8 @@ namespace Kurmann.Videoschnitt.CommonServices.FileSystem;
 
 public interface IFileOperations
 {
-    void SaveFile(XDocument document, string path);
-    void MoveFile(string sourcePath, string destinationPath, bool overwrite = false);
-    void CopyFile(string sourcePath, string destinationPath, bool overwrite = false);
+    void SaveFile(XDocument document, string path, bool inheritPermissions = true);
+    void MoveFile(string sourcePath, string destinationPath, bool overwrite = false, bool inheritPermissions = true);
+    void CopyFile(string sourcePath, string destinationPath, bool overwrite = false, bool inheritPermissions = true);
     string ReadFile(string path);
 }
