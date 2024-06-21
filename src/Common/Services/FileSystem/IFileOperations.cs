999af00e1dff7fdb54abc72781cdb6aa3b3e3f92
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-06-22 00:05:37 +0200

    Async method with Async method suffix

--- src/Common/Services/FileSystem/IFileOperations.cs
+++ src/Common/Services/FileSystem/IFileOperations.cs
@@ -6,8 +6,8 @@ namespace Kurmann.Videoschnitt.Common.Services.FileSystem;
 public interface IFileOperations
 {
     Task<Result> SaveFile(XDocument document, string path, bool inheritPermissions = true);
-    Task<Result> MoveFile(string sourcePath, string destinationPath, bool overwrite = false, bool inheritPermissions = true);
-    Task<Result> CopyFile(string sourcePath, string destinationPath, bool overwrite = false, bool inheritPermissions = true);
+    Task<Result> MoveFileAsync(string sourcePath, string destinationPath, bool overwrite = false, bool inheritPermissions = true);
+    Task<Result> CopyFileAsync(string sourcePath, string destinationPath, bool overwrite = false, bool inheritPermissions = true);
     Result<string> ReadFile(string path);
-    Task<Result> CreateDirectory(string path, bool inheritPermissions = true);
+    Task<Result> CreateDirectoryAsync(string path, bool inheritPermissions = true);
 }
