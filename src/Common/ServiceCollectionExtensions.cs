8dcea6821f17df78e3a9c7553ddbe57484e5c52f
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-06-22 16:39:15 +0200

    integrate external tools settings to common assembly

--- src/Common/ServiceCollectionExtensions.cs
+++ src/Common/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Kurmann.Videoschnitt.Common.Services.FileSystem;
 using Kurmann.Videoschnitt.Common.Services.FileSystem.Unix;
 
@@ -6,8 +7,11 @@ namespace Kurmann.Videoschnitt.Common;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddCommonServicesEngine(this IServiceCollection services)
-    {   
+    public static IServiceCollection AddCommonServicesEngine(this IServiceCollection services, IConfiguration configuration)
+    { 
+        // Add configuration sources
+        services.Configure<ApplicationSettings>(options => configuration.GetSection(ApplicationSettings.SectionName).Bind(options));
+
         // Register Engine
         services.AddScoped<ExecuteCommandService>();
         services.AddScoped<IFileOperations, FileOperations>();
