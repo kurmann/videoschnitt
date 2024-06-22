391de29e9a525e3f6c0afb1f17bf23acce9f06b6
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-06-22 17:13:52 +0200

    Integrate SIPS full path from application settigns into MacOSColorConversionService

--- src/Common/Services/ImageProcessing/MacOS/MacOSColorConversionService.cs
+++ src/Common/Services/ImageProcessing/MacOS/MacOSColorConversionService.cs
@@ -1,5 +1,6 @@
 using CSharpFunctionalExtensions;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace Kurmann.Videoschnitt.Common.Services.ImageProcessing.MacOS;
 
@@ -7,21 +8,38 @@ public class MacOSColorConversionService : IColorConversionService
 {
     private readonly ExecuteCommandService _executeCommandService;
     private readonly ILogger<MacOSColorConversionService> _logger;
+    
+    private readonly string _sipsCommand;
+    private const string DefaultSipsCommand = "sips";
+    private const string DefaultOutputColorSpace = "adobe_rgb";
+    private const string DefaultInputColorSpace = "bt2020";
 
-    public MacOSColorConversionService(ExecuteCommandService executeCommandService, ILogger<MacOSColorConversionService> logger)
+    public MacOSColorConversionService(ExecuteCommandService executeCommandService, ILogger<MacOSColorConversionService> logger, IOptions<ApplicationSettings> applicationSettings)
     {
         _executeCommandService = executeCommandService;
         _logger = logger;
+
+        // Prüfe ob ein vollständiger Pfad von SIPS übergeben wurde, ansonsten nimm an, dass die Umgebungsvariable gesetzt ist. Gib eine Warnung aus, wenn die Umgebungsvariable nicht gesetzt ist.
+        if (applicationSettings.Value.ExternalTools?.Sips?.Path == null)
+        {
+            _logger.LogWarning("SIPS-Pfad nicht gesetzt. Es wird angenommen, dass SIPS in der Umgebungsvariable PATH gesetzt ist.");
+        }
+        else
+        {
+            _logger.LogInformation($"SIPS-Pfad: {applicationSettings.Value.ExternalTools?.Sips?.Path}");
+        }
+
+        _sipsCommand = applicationSettings.Value.ExternalTools?.Sips?.Path ?? DefaultSipsCommand;
     }
 
-    public async Task<Result> ConvertColorSpaceAsync(string inputFilePath, string outputFilePath, string inputColorSpace = "bt2020", string outputColorSpace = "adobe_rgb")
+    public async Task<Result> ConvertColorSpaceAsync(string inputFilePath, string outputFilePath, string inputColorSpace = DefaultInputColorSpace, string outputColorSpace = DefaultOutputColorSpace)
     {
         _logger.LogInformation($"Konvertiere Farbraum von {inputColorSpace} nach {outputColorSpace} für: {inputFilePath}");
         _logger.LogInformation("Hinweis: Der Eingangsfarbraum wird ignoriert, da sips diesen nicht benötigt um den Farbraum zu konvertieren.");
 
-        if (outputColorSpace != "adobe_rgb")
+        if (outputColorSpace != DefaultOutputColorSpace)
         {
-            return Result.Failure("Der Ausgangsfarbraum muss 'adobe_rgb' sein, da sips der MacOSColorConversionService nur diese Umwandlung unterstützt.");
+            return Result.Failure($"Der Ausgangsfarbraum muss '{DefaultOutputColorSpace}' sein, da sips der MacOSColorConversionService nur diese Umwandlung unterstützt.");
         }
 
         if (string.IsNullOrWhiteSpace(inputFilePath))
@@ -37,8 +55,8 @@ public class MacOSColorConversionService : IColorConversionService
         _logger.LogInformation($"Wandle Farbraum von {inputColorSpace} nach {outputColorSpace} um: {inputFilePath}");
 
         var arguments = $"-m /System/Library/ColorSync/Profiles/AdobeRGB1998.icc \"{inputFilePath}\" --out \"{outputFilePath}\"";
-        _logger.LogInformation($"sips-Befehl: sips {arguments}");
-        var result = await _executeCommandService.ExecuteCommandAsync("sips", arguments);
+        _logger.LogInformation($"SIPS-Befehl: {_sipsCommand} {arguments}");
+        var result = await _executeCommandService.ExecuteCommandAsync(_sipsCommand, arguments);
 
         if (result.IsSuccess)
         {
