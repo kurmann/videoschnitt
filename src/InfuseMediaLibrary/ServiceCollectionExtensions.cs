9e8a7d9d75dbbefb93ac3557f7bee857567565e6
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-06-22 08:03:04 +0200

    Register MacOSColorConversionService for image processing

--- src/InfuseMediaLibrary/ServiceCollectionExtensions.cs
+++ src/InfuseMediaLibrary/ServiceCollectionExtensions.cs
@@ -4,6 +4,8 @@ using Kurmann.Videoschnitt.InfuseMediaLibrary.Services;
 using Kurmann.Videoschnitt.Common;
 using Kurmann.Videoschnitt.Common.Services.FileSystem;
 using Kurmann.Videoschnitt.Common.Services.FileSystem.Unix;
+using Kurmann.Videoschnitt.Common.Services.ImageProcessing;
+using Kurmann.Videoschnitt.Common.Services.ImageProcessing.MacOS;
 
 namespace Kurmann.Videoschnitt.InfuseMediaLibrary;
 
@@ -20,6 +22,7 @@ public static class ServiceCollectionExtensions
         services.AddScoped<InfuseMetadataXmlService>();
         services.AddScoped<MediaIntegratorService>();
         services.AddScoped<IFileOperations, FileOperations>();
+        services.AddScoped<IColorConversionService, MacOSColorConversionService>();
         services.AddScoped<PosterAndFanartService>();
 
         services.AddCommonServicesEngine();
