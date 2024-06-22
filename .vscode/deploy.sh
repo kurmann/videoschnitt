3caa52b19468275a97891830c9360bfa1624ef9f
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-06-22 22:31:41 +0200

    New paths and sub directories defined in the deploy.sh

--- .vscode/deploy.sh
+++ .vscode/deploy.sh
@@ -9,18 +9,21 @@ fi
 # Pfad zum Projekt
 PROJECT_PATH=$1
 
-# Pfad zur Anwendung
-APP_PATH="/usr/local/bin/Kurmann.Videoschnitt"
+# Verzeichnis für die Anwendung
+APP_DIR="/usr/local/kurmann-videoschnitt"
 
 # Erklärung für das Admin-Passwort
 echo "Das Admin-Passwort wird benötigt, um die Anwendung in ein systemweites Verzeichnis zu installieren und den LaunchDaemon-Dienst neu zu starten."
 
+# Erstelle das Verzeichnis, falls es nicht existiert
+sudo mkdir -p $APP_DIR/Application
+
 # Anwendung veröffentlichen
 echo "Veröffentliche die .NET-Anwendung als Single-File..."
-sudo dotnet publish $PROJECT_PATH/src/Application/Application.csproj -c Release -r osx-x64 --self-contained -p:PublishSingleFile=true -p:DebugType=None -o /usr/local/bin
+sudo dotnet publish $PROJECT_PATH/src/Application/Application.csproj -c Release -o $APP_DIR/Application
 
 if [ $? -eq 0 ]; then
-  echo "Veröffentlichung erfolgreich. Die Anwendung wurde nach $APP_PATH deployed."
+  echo "Veröffentlichung erfolgreich. Die Anwendung wurde nach $APP_DIR/Application deployed."
 else
   echo "Fehler bei der Veröffentlichung."
   exit 1
@@ -33,7 +36,7 @@ sudo launchctl load /Library/LaunchDaemons/com.swiss.kurmann.videoschnitt.plist
 
 if [ $? -eq 0 ]; then
   echo "LaunchDaemon-Dienst erfolgreich neu geladen."
-  echo "Die Anwendung läuft jetzt unter $APP_PATH"
+  echo "Die Anwendung läuft jetzt unter $APP_DIR/Application"
 else
   echo "Fehler beim Neuladen des LaunchDaemon-Dienstes."
   exit 1
