813d53d27c99594307dd5a204442adb949870afa
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-06-29 23:06:00 +0200

    chore: Update deploy.sh to delete and recreate application directory

--- .vscode/deploy.sh
+++ .vscode/deploy.sh
@@ -15,12 +15,13 @@ USER_NAME=$USER
 # Verzeichnis für die Anwendung
 APP_DIR="/Users/${USER_NAME}/bin/Kurmann/Videoschnitt"
 
-# Erstelle das Verzeichnis, falls es nicht existiert
+# Lösche und erstelle das Verzeichnis neu für die Anwendung
+rm -rf "$APP_DIR"
 mkdir -p "$APP_DIR"
 
 # Anwendung veröffentlichen ohne Single-File und Debug-Dateien
 echo "Veröffentliche die .NET-Anwendung..."
-dotnet publish "$PROJECT_PATH/src/Application/Application.csproj" -c Release -r osx-x64 --self-contained -p:DebugType=None -o "$APP_DIR"
+dotnet publish "$PROJECT_PATH/src/Application/Application.csproj" -c Release --self-contained -p:DebugType=None -o "$APP_DIR"
 
 if [ $? -eq 0 ]; then
   echo "Veröffentlichung erfolgreich. Die Anwendung wurde nach $APP_DIR deployed."
