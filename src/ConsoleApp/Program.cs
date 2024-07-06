d91b630b3f6d8bab5f979624f3f3bdcf1c8dde3c
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-07-06 22:34:19 +0200

    Do not run console app after finished workflow

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -1,88 +1,92 @@
-﻿using System.Globalization;
+﻿using System.Diagnostics;
+using System.Globalization;
 using CommandLine;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Kurmann.Videoschnitt.Workflows;
 
-namespace Kurmann.Videoschnitt.ConsoleApp;
-
-public class Program
+namespace Kurmann.Videoschnitt.ConsoleApp
 {
-    public static async Task Main(string[] args)
+    public class Program
     {
-        var host = CreateHostBuilder(args).Build();
-        var logger = host.Services.GetRequiredService<ILogger<Program>>();
-
-        CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
-        CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
-
-        Parser.Default.ParseArguments<Options>(args)
-            .WithParsed(async opts => await RunOptions(opts, host.Services, logger))
-            .WithNotParsed(errs => HandleParseError(errs, logger));
-
-        await host.RunAsync();
-    }
-
-    private static async Task RunOptions(Options opts, IServiceProvider services, ILogger logger)
-    {
-        if (opts.Workflow == HealthCheckWorkflow.WorkflowName)
+        public static async Task Main(string[] args)
         {
-            logger.LogInformation("Starting FinalCutPro workflow.");
-            var workflow = services.GetRequiredService<HealthCheckWorkflow>();
-            var result = workflow.Execute();
-            if (result.IsSuccess)
+            var stopwatch = Stopwatch.StartNew();
+
+            var host = CreateHostBuilder(args).Build();
+            var logger = host.Services.GetRequiredService<ILogger<Program>>();
+
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+            CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
+
+            var exitCode = Parser.Default.ParseArguments<Options>(args)
+                .MapResult(
+                    async opts => await RunOptions(opts, host.Services, logger),
+                    errs => Task.FromResult(1)
+                );
+
+            await host.StopAsync();
+
+            stopwatch.Stop();
+            logger.LogInformation("Total execution time: {ElapsedMilliseconds} ms", stopwatch.ElapsedMilliseconds);
+
+            Environment.Exit(await exitCode);
+        }
+
+        private static async Task<int> RunOptions(Options opts, IServiceProvider services, ILogger logger)
+        {
+            if (opts.Workflow == HealthCheckWorkflow.WorkflowName)
             {
-                logger.LogInformation("FinalCutPro workflow completed successfully.");
+                logger.LogInformation("Starting HealthCheck workflow.");
+                var workflow = services.GetRequiredService<HealthCheckWorkflow>();
+                var result = workflow.Execute();
+                if (result.IsSuccess)
+                {
+                    logger.LogInformation("HealthCheck workflow completed successfully.");
+                    return 0; // success
+                }
+                else
+                {
+                    logger.LogError("Error in HealthCheck workflow: {Error}", result.Error);
+                    return 1; // error
+                }
+            }
+            else if (opts.Workflow == "FinalCutPro")
+            {
+                logger.LogInformation("Starting FinalCutPro workflow.");
+                var workflow = services.GetRequiredService<FinalCutProWorkflow>();
+                var result = await workflow.ExecuteAsync();
+                if (result.IsSuccess)
+                {
+                    logger.LogInformation("FinalCutPro workflow completed successfully.");
+                    return 0; // success
+                }
+                else
+                {
+                    logger.LogError("Error in FinalCutPro workflow: {Error}", result.Error);
+                    return 1; // error
+                }
             }
             else
             {
-                logger.LogError("Error in FinalCutPro workflow: {Error}", result.Error);
+                logger.LogWarning("No valid workflow specified.");
+                return 1; // invalid workflow
             }
         }
-        // if (opts.Workflow == "FinalCutPro")
-        // {
-        //     logger.LogInformation("Starting FinalCutPro workflow.");
-        //     var workflow = services.GetRequiredService<FinalCutProWorkflow>();
-        //     var result = await workflow.ExecuteAsync();
-        //     if (result.IsSuccess)
-        //     {
-        //         logger.LogInformation("FinalCutPro workflow completed successfully.");
-        //     }
-        //     else
-        //     {
-        //         logger.LogError("Error in FinalCutPro workflow: {Error}", result.Error);
-        //     }
-        // }
-        else
-        {
-            logger.LogWarning("No valid workflow specified.");
-        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddLogging(configure => configure.AddConsole());
+                    services.AddWorkflows();
+                });
     }
 
-    private static void HandleParseError(IEnumerable<Error> errs, ILogger logger)
+    public class Options
     {
-        // Handle errors
-        logger.LogError("Error parsing arguments.");
-
-        foreach (var error in errs)
-        {
-            logger.LogError("Error: {Error}", error);
-        }
+        [Option('w', "workflow", Required = true, HelpText = "Name of the workflow to execute.")]
+        public string? Workflow { get; set; }
     }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureServices((hostContext, services) =>
-            {
-                services.AddLogging(configure => configure.AddConsole());
-
-                services.AddWorkflows();
-            });
-}
-
-public class Options
-{
-    [Option('w', "workflow", Required = true, HelpText = "Name of the workflow to execute.")]
-    public string? Workflow { get; set; }
 }
