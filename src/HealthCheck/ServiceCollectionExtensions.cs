3993f3777906c81d717fed674cbf2cc664c87c7e
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-07-06 21:33:29 +0200

    feat: Add HealthCheck service collection extensions

---
+++ src/HealthCheck/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+using Kurmann.Videoschnitt.HealthCheck.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kurmann.Videoschnitt.HealthCheck;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddHealthCheck(this IServiceCollection services)
+    {   
+        services.AddScoped<HealthCheckFeature>();
+        services.AddScoped<ToolsVersionService>();
+
+        return services;
+    }
+}
