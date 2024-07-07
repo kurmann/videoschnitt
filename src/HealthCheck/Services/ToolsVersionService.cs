aa9a63522fae2520572ddc7549ebd0926fe147dc
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-07-07 10:59:08 +0200

    feat: Update ToolsVersionService to include GetSipsVersion method
    This commit adds a new method `GetSipsVersion` to the `ToolsVersionService` class. This method executes the command `sips --version` and returns the version of SIPS, a tool used for image processing on macOS. This enhancement improves the functionality of the service by providing the ability to retrieve the SIPS version.

--- src/HealthCheck/Services/ToolsVersionService.cs
+++ src/HealthCheck/Services/ToolsVersionService.cs
@@ -14,6 +14,10 @@ public class ToolsVersionService
         _logger = logger;
     }
 
+    /// <summary>
+    /// Führt den Befehl `ffmpeg -version` aus und gibt die Version zurück.
+    /// </summary>
+    /// <returns></returns>
     public Result<string> GetFFmpegVersion()
     {
         try
@@ -26,6 +30,41 @@ public class ToolsVersionService
                 {
                     FileName = "/bin/bash",
                     Arguments = $"-c \"{ffmpegPath} -version\"",
+                    RedirectStandardOutput = true,
+                    StandardOutputEncoding = Encoding.UTF8,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+
+            _logger.LogInformation("Executing command: {Command}", process.StartInfo.Arguments);
+
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return Result.Success(result);
+        }
+        catch (Exception ex)
+        {
+            return Result.Failure<string>(ex.Message);
+        }
+    }
+
+    /// <summary>
+    /// Führt den Befehl `sips --version`aus und gibt die Version zurück.
+    /// SIPS ist ein Tool, das auf macOS verfügbar ist und für die Bildbearbeitung verwendet wird.
+    /// </summary>
+    /// <returns></returns>
+    public Result<string> GetSipsVersion()
+    {
+        try
+        {
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"sips --version\"",
                     RedirectStandardOutput = true,
                     StandardOutputEncoding = Encoding.UTF8,
                     UseShellExecute = false,
