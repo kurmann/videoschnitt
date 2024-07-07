8a3d0067ea3a519d9d748a3df54856b8ecc384f5
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-07-07 12:24:21 +0200

    Pass configuration from workflows to the feature assemblies

--- src/Workflows/ServiceCollectionExtensions.cs
+++ src/Workflows/ServiceCollectionExtensions.cs
@@ -1,12 +1,20 @@
 using Microsoft.Extensions.DependencyInjection;
 using Kurmann.Videoschnitt.HealthCheck;
+using Microsoft.Extensions.Configuration;
+using Kurmann.Videoschnitt.MetadataProcessor;
+using Kurmann.Videoschnitt.InfuseMediaLibrary;
 
 namespace Kurmann.Videoschnitt.Workflows;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddWorkflows(this IServiceCollection services)
-    {   
+    public static IServiceCollection AddWorkflows(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Add configuration sources to the DI container
+        services.AddMetadataProcessor(configuration);
+        services.AddInfuseMediaLibrary(configuration);
+
+        // Register workflows
         services.AddScoped<HealthCheckWorkflow>();
         services.AddScoped<FinalCutProWorkflow>();
 
