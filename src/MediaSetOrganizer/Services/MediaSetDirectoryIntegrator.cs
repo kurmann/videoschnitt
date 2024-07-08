f1f5608d4f773eda2f9fdbda4de7953b605773f4
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-07-08 09:33:21 +0200

    IntegrateMediaServerFiles as private method

--- src/MediaSetOrganizer/Services/MediaSetDirectoryIntegrator.cs
+++ src/MediaSetOrganizer/Services/MediaSetDirectoryIntegrator.cs
@@ -54,42 +54,53 @@ public class MediaSetDirectoryIntegrator
                 }
             }
 
-            _logger.LogInformation("Verschiebe Medien-Dateien in das Medienset-Verzeichnis: {mediaSetDirectory}", mediaSetTargetDirectory.FullName);
-            if (mediaSet.LocalMediaServerVideoFile.HasNoValue)
+            var integrateMediaServerFilesResult = await IntegrateMediaServerFiles(mediaSet, mediaSetTargetDirectory);
+            if (integrateMediaServerFilesResult.IsFailure)
             {
-                _logger.LogInformation("Keine Medien-Dateien für den Medienserver vorhanden.");
-            }
-            else
-            {
-                var videoFileForMediaServer = mediaSet.LocalMediaServerVideoFile.Value;
-                var videoFileForMediaServerTargetPath = Path.Combine(mediaSetTargetDirectory.FullName,
-                                                                     _mediaSetOrganizerSettings.MediaSet.MediaServerFilesSubDirectoryName,
-                                                                     videoFileForMediaServer.FileInfo.Name);
-                _logger.LogInformation("Verschiebe Video-Datei für Medienserver: {videoFileForMediaServerTargetPath}", videoFileForMediaServerTargetPath);
-
-                // Prüfe ob das Unterzielverzeichnis für die Medienserver-Dateien existiert
-                var mediaServerFilesSubDirectory = new DirectoryInfo(Path.Combine(mediaSetTargetDirectory.FullName, _mediaSetOrganizerSettings.MediaSet.MediaServerFilesSubDirectoryName));
-                if (!mediaServerFilesSubDirectory.Exists)
-                {
-                    _logger.LogInformation("Erstelle Unterverzeichnis für Medienserver-Dateien: {mediaServerFilesSubDirectory}", mediaServerFilesSubDirectory.FullName);
-                    var mediaServerFilesSubDirectoryCreateResult = await _fileOperations.CreateDirectoryAsync(mediaServerFilesSubDirectory.FullName);
-                    if (mediaServerFilesSubDirectoryCreateResult.IsFailure)
-                    {
-                        return Result.Failure<List<DirectoryInfo>>($"Fehler beim Erstellen des Unterverzeichnisses für Medienserver-Dateien: {mediaServerFilesSubDirectoryCreateResult.Error}");
-                    }
-                }
-
-                var videoFileForMediaServerMoveResult = await _fileOperations.MoveFileAsync(videoFileForMediaServer.FileInfo.FullName, videoFileForMediaServerTargetPath, true);
-                if (videoFileForMediaServerMoveResult.IsFailure)
-                {
-                    return Result.Failure<List<DirectoryInfo>>($"Fehler beim Verschieben der Video-Datei für den Medienserver: {videoFileForMediaServerMoveResult.Error}");
-                }
-                _logger.LogInformation("Video-Datei für Medienserver erfolgreich verschoben.");
+                return Result.Failure<List<DirectoryInfo>>($"Fehler beim Integrieren der Medienserver-Dateien: {integrateMediaServerFilesResult.Error}");
             }
 
             mediaSetDirectories.Add(mediaSetTargetDirectory);
         }
 
         return Result.Success(mediaSetDirectories);
+    }
+
+    private async Task<Result<Maybe<DirectoryInfo>>> IntegrateMediaServerFiles(MediaSet mediaSet, DirectoryInfo mediaSetTargetDirectory)
+    {
+        _logger.LogInformation("Verschiebe Medien-Dateien in das Medienset-Verzeichnis: {mediaSetDirectory}", mediaSetTargetDirectory.FullName);
+        if (mediaSet.LocalMediaServerVideoFile.HasNoValue)
+        {
+            _logger.LogInformation("Keine Medien-Dateien für den Medienserver vorhanden.");
+            return Result.Success(Maybe<DirectoryInfo>.None);
+        }
+        else
+        {
+            var videoFileForMediaServer = mediaSet.LocalMediaServerVideoFile.Value;
+            var videoFileForMediaServerTargetPath = Path.Combine(mediaSetTargetDirectory.FullName,
+                                                                 _mediaSetOrganizerSettings.MediaSet.MediaServerFilesSubDirectoryName,
+                                                                 videoFileForMediaServer.FileInfo.Name);
+            _logger.LogInformation("Verschiebe Video-Datei für Medienserver: {videoFileForMediaServerTargetPath}", videoFileForMediaServerTargetPath);
+
+            // Prüfe ob das Unterzielverzeichnis für die Medienserver-Dateien existiert
+            var mediaServerFilesSubDirectory = new DirectoryInfo(Path.Combine(mediaSetTargetDirectory.FullName, _mediaSetOrganizerSettings.MediaSet.MediaServerFilesSubDirectoryName));
+            if (!mediaServerFilesSubDirectory.Exists)
+            {
+                _logger.LogInformation("Erstelle Unterverzeichnis für Medienserver-Dateien: {mediaServerFilesSubDirectory}", mediaServerFilesSubDirectory.FullName);
+                var mediaServerFilesSubDirectoryCreateResult = await _fileOperations.CreateDirectoryAsync(mediaServerFilesSubDirectory.FullName);
+                if (mediaServerFilesSubDirectoryCreateResult.IsFailure)
+                {
+                    return Result.Failure<Maybe<DirectoryInfo>>($"Fehler beim Erstellen des Unterverzeichnisses für Medienserver-Dateien: {mediaServerFilesSubDirectoryCreateResult.Error}");
+                }
+            }
+
+            var videoFileForMediaServerMoveResult = await _fileOperations.MoveFileAsync(videoFileForMediaServer.FileInfo.FullName, videoFileForMediaServerTargetPath, true);
+            if (videoFileForMediaServerMoveResult.IsFailure)
+            {
+                return Result.Failure<Maybe<DirectoryInfo>>($"Fehler beim Verschieben der Video-Datei für den Medienserver: {videoFileForMediaServerMoveResult.Error}");
+            }
+            _logger.LogInformation("Video-Datei für Medienserver erfolgreich verschoben.");
+            return Maybe<DirectoryInfo>.From(mediaServerFilesSubDirectory);
+        }
     }
 }
