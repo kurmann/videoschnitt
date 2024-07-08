71112f105c99e4cf9ac48a614ed520b5c4a6abed
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-07-08 19:20:08 +0200

    ExpandHomeDirectory (interpreting tilde)

--- src/ConfigurationModule/Settings/ApplicationSettings.cs
+++ src/ConfigurationModule/Settings/ApplicationSettings.cs
@@ -1,109 +1,72 @@
-namespace Kurmann.Videoschnitt.ConfigurationModule.Settings;
+using System;
+using System.IO;
 
-/// <summary>
-/// Einstellungen, die für die ganze Anwendung gelten.
-/// </summary>
-public class ApplicationSettings
+namespace Kurmann.Videoschnitt.ConfigurationModule.Settings
 {
-    public const string SectionName = "Application";
+    public class ApplicationSettings
+    {
+        public const string SectionName = "Application";
 
-    /// <summary>
-    /// Der Pfad zur Infuse-Mediathek.
-    /// </summary>
-    public string InfuseMediaLibraryPath { get; set; } = DefaultInfuseMediaLibraryPath;
-    public const string InfuseMediaLibraryPathConfigKey = $"{SectionName}:InfuseMediaLibraryPath";
-    public const string DefaultInfuseMediaLibraryPath = "~/Movies/Infuse Media Library";
-    public bool IsDefaultInfuseMediaLibraryPath => InfuseMediaLibraryPath == DefaultInfuseMediaLibraryPath;
+        public string InfuseMediaLibraryPath { get; set; } = ExpandHomeDirectory(DefaultInfuseMediaLibraryPath);
+        public const string InfuseMediaLibraryPathConfigKey = $"{SectionName}:InfuseMediaLibraryPath";
+        public const string DefaultInfuseMediaLibraryPath = "~/Movies/Infuse Media Library";
+        public bool IsDefaultInfuseMediaLibraryPath => InfuseMediaLibraryPath == DefaultInfuseMediaLibraryPath;
 
-    /// <summary>
-    /// Eingangsverzeichnis, in dem die zu verarbeitenden Dateien liegen.
-    /// </summary>
-    public string InputDirectory { get; set; } = DefaultInputDirectory;
-    public const string InputDirectoryConfigKey = $"{SectionName}:InputDirectory";
-    public const string DefaultInputDirectory = "~/Movies/Final Cut Export";
-    public bool IsDefaultInputDirectory => InputDirectory == DefaultInputDirectory;
+        public string InputDirectory { get; set; } = ExpandHomeDirectory(DefaultInputDirectory);
+        public const string InputDirectoryConfigKey = $"{SectionName}:InputDirectory";
+        public const string DefaultInputDirectory = "~/Movies/Final Cut Export";
+        public bool IsDefaultInputDirectory => InputDirectory == DefaultInputDirectory;
 
-    /// <summary>
-    /// Der Pfad zum lokalen Medienset-Verzeichnis.
-    /// </summary>
-    /// <value></value>
-    public string MediaSetPathLocal { get; set; } = DefaultMediaSetPathLocal;
-    public const string MediaSetPathLocalConfigKey = $"{SectionName}:MediaSetPathLocal";
-    public const string DefaultMediaSetPathLocal = "~/Movies/MediaSets";
-    public bool IsDefaultMediaSetPathLocal => MediaSetPathLocal == DefaultMediaSetPathLocal;
+        public string MediaSetPathLocal { get; set; } = ExpandHomeDirectory(DefaultMediaSetPathLocal);
+        public const string MediaSetPathLocalConfigKey = $"{SectionName}:MediaSetPathLocal";
+        public const string DefaultMediaSetPathLocal = "~/Movies/MediaSets";
+        public bool IsDefaultMediaSetPathLocal => MediaSetPathLocal == DefaultMediaSetPathLocal;
 
-    /// <summary>
-    /// Der Pfad zum Verzeichnis, in dem die exportierten Final Cut Pro-Dateien liegen.
-    /// </summary>
-    /// <value></value>
-    public string FinalCutExportDirectory { get; set; } = DefaultFinalCutExportDirectory;
-    public const string FinalCutExportDirectoryConfigKey = $"{SectionName}:FinalCutExportDirectory";
-    public const string DefaultFinalCutExportDirectory = "~/Movies/Final Cut Export";
-    public bool IsDefaultFinalCutExportDirectory => FinalCutExportDirectory == DefaultFinalCutExportDirectory;
+        public string FinalCutExportDirectory { get; set; } = ExpandHomeDirectory(DefaultFinalCutExportDirectory);
+        public const string FinalCutExportDirectoryConfigKey = $"{SectionName}:FinalCutExportDirectory";
+        public const string DefaultFinalCutExportDirectory = "~/Movies/Final Cut Export";
+        public bool IsDefaultFinalCutExportDirectory => FinalCutExportDirectory == DefaultFinalCutExportDirectory;
 
-    /// <summary>
-    /// Der Pfad zum lokalen Infuse-Mediathek-Verzeichnis.
-    /// </summary>
-    /// <value></value>
-    public string InfuseMediaLibraryPathLocal { get; set; } = DefaultInfuseMediaLibraryPathLocal;
-    public const string InfuseMediaLibraryPathLocalConfigKey = $"{SectionName}:InfuseMediaLibraryPathLocal";
-    public const string DefaultInfuseMediaLibraryPathLocal = "~/Movies/Infuse Media Library";
-    public bool IsDefaultInfuseMediaLibraryPathLocal => InfuseMediaLibraryPathLocal == DefaultInfuseMediaLibraryPathLocal;
+        public string InfuseMediaLibraryPathLocal { get; set; } = ExpandHomeDirectory(DefaultInfuseMediaLibraryPathLocal);
+        public const string InfuseMediaLibraryPathLocalConfigKey = $"{SectionName}:InfuseMediaLibraryPathLocal";
+        public const string DefaultInfuseMediaLibraryPathLocal = "~/Movies/Infuse Media Library";
+        public bool IsDefaultInfuseMediaLibraryPathLocal => InfuseMediaLibraryPathLocal == DefaultInfuseMediaLibraryPathLocal;
 
-    /// <summary>
-    /// Der Pfad zum Medienset-Verzeichnis auf dem externen Archiv oder Medienserver.
-    /// </summary>
-    /// <value></value>
-    public string? MediaSetPathRemote { get; set; }
-    public const string MediaSetPathRemoteConfigKey = $"{SectionName}:MediaSetPathRemote";
+        public string? MediaSetPathRemote { get; set; }
+        public const string MediaSetPathRemoteConfigKey = $"{SectionName}:MediaSetPathRemote";
 
-    /// <summary>
-    /// Die Einstellungen für externe Tools wie FFmpeg oder SIPS.
-    /// </summary>
-    /// <returns></returns>
-    public ExternalToolsSettings ExternalTools { get; set; } = new ExternalToolsSettings();
-}
+        public ExternalToolsSettings ExternalTools { get; set; } = new ExternalToolsSettings();
 
-public class ExternalToolsSettings
-{
-    /// <summary>
-    /// Einstellungen für das FFmpeg-Tool.
-    /// </summary>
-    /// <returns></returns>
-    public FFMmpegSettings FFMpeg { get; set; } = new FFMmpegSettings();
+        public static string ExpandHomeDirectory(string path)
+        {
+            if (path.StartsWith('~'))
+            {
+                var homeDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                return path.Replace("~", homeDirectory);
+            }
+            return path;
+        }
+    }
 
-    /// <summary>
-    /// Einstellungen für das FFProbe-Tool.
-    /// </summary>
-    /// <returns></returns>
-    public FFProbeSettings FFProbe { get; set; } = new FFProbeSettings();
+    public class ExternalToolsSettings
+    {
+        public FFMmpegSettings FFMpeg { get; set; } = new FFMmpegSettings();
+        public FFProbeSettings FFProbe { get; set; } = new FFProbeSettings();
+        public SipsSettings Sips { get; set; } = new SipsSettings();
+    }
 
-    /// <summary>
-    /// Einstellungen für das SIPS-Tool auf MacOS.
-    /// </summary>
-    /// <returns></returns>
-    public SipsSettings Sips { get; set; } = new SipsSettings();
-}
+    public class FFMmpegSettings
+    {
+        public string Path { get; set; } = "/usr/local/bin/ffmpeg";
+    }
 
-public class FFMmpegSettings
-{
-    /// <summary>
-    /// Der absolute Pfad zu FFMpeg.
-    /// </summary>
-    /// <value></value>
-    public string Path { get; set; } = "/usr/local/bin/ffmpeg";
-}
+    public class FFProbeSettings
+    {
+        public string Path { get; set; } = "/opt/local/bin/ffprobe";
+    }
 
-public class FFProbeSettings
-{
-    public string Path { get; set; } = "/opt/local/bin/ffprobe";
-}
-
-public class SipsSettings
-{
-    /// <summary>
-    /// Der absolute Pfad zu SIPS.
-    /// </summary>
-    /// <value></value>
-    public string Path { get; set; } = "/usr/bin/sips";
+    public class SipsSettings
+    {
+        public string Path { get; set; } = "/usr/bin/sips";
+    }
 }
