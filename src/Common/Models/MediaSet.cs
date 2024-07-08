3b9c200e3be1120f8ca376d5f823b4edc30b5f72
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-07-08 14:32:56 +0200

    feat: Add Masterfile property to MediaSet model

--- src/Common/Models/MediaSet.cs
+++ src/Common/Models/MediaSet.cs
@@ -14,14 +14,12 @@ public record MediaSet
 {
     public string? Title { get; init; }
 
-    /// <summary>
-    /// Videodateien
-    /// </summary>
-    /// <value></value>
     public Maybe<SupportedVideo> LocalMediaServerVideoFile { get; init;}
 
     public Maybe<List<SupportedVideo>> InternetStreamingVideoFiles { get; init; }
 
     public Maybe<List<SupportedImage>> ImageFiles { get; init; }
+
+    public Maybe<Masterfile> Masterfile { get; set; }
 }
 
