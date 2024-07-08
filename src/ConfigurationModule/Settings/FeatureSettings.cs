02766d61c7f4a74a766846455f6db7d690d6b01a
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-07-08 07:53:15 +0200

    removed obsolete FeatureSettings

--- src/ConfigurationModule/Settings/FeatureSettings.cs
+++
@@ -1,7 +0,0 @@
-namespace Kurmann.Videoschnitt.ConfigurationModule.Settings;
-
-public record FeatureSettings
-{
-    public InfuseMediaLibrarySettings InfuseMediaLibrarySettings { get; init; } = new InfuseMediaLibrarySettings();
-    public MediaSetOrganizerSettings MetadataProcessingSettings { get; init; } = new MediaSetOrganizerSettings();
-}
