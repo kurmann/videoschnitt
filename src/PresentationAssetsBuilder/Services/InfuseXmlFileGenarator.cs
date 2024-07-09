889fc48c9382bf69b57b0fdae2726d8c7ba1ea7a
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-07-09 19:49:40 +0200

    Gather all supported media files from input directory

--- src/PresentationAssetsBuilder/Services/InfuseXmlFileGenarator.cs
+++ src/PresentationAssetsBuilder/Services/InfuseXmlFileGenarator.cs
@@ -1,5 +1,8 @@
 using CSharpFunctionalExtensions;
+using Kurmann.Videoschnitt.Common.Entities.MediaTypes;
+using Kurmann.Videoschnitt.ConfigurationModule.Settings;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace Kurmann.Videoschnitt.PresentationAssetsBuilder.Services;
 
@@ -9,17 +12,33 @@ namespace Kurmann.Videoschnitt.PresentationAssetsBuilder.Services;
 public class InfuseXmlFileGenarator
 {
     private readonly ILogger<InfuseXmlFileGenarator> _logger;
+    private readonly MediaSetOrganizerSettings _mediaSetOrganizerSettings;
 
-    public InfuseXmlFileGenarator(ILogger<InfuseXmlFileGenarator> logger)
+    public InfuseXmlFileGenarator(ILogger<InfuseXmlFileGenarator> logger, IOptions<MediaSetOrganizerSettings> applicationSettings)
     {
         _logger = logger;
+        _mediaSetOrganizerSettings = applicationSettings.Value;
     }
 
     public Task<Result<List<FileInfo>>> GenerateInfuseXmlFiles(string inputDirectory)
     {
         // Lese alle Verzeichnisse im Eingabeverzeichnis und nimm an, dass es sich um Mediensets handelt
         var mediaSetDirectoryInfos = new DirectoryInfo(inputDirectory).GetDirectories().ToList();
-        _logger.LogInformation("Found {mediaSetDirectoryInfos.Count} media sets in {inputDirectory}", mediaSetDirectoryInfos.Count, inputDirectory);
+        _logger.LogInformation("Es wurden {mediaSetDirectoryInfos.Count} Mediensets gefunden", mediaSetDirectoryInfos.Count);
+
+        // Ziel ist es in jedem Mediaset zu jeder Videodatei eine Infuse-XML-Datei zu erstellen
+        foreach (var mediaSetDirectoryInfo in mediaSetDirectoryInfos)
+        {
+            // Filtere alle Videodateien mit dem richtigen Dateiendungen einschließlich Unterverzeichnisse
+            var supportedMediaByMediaSet = mediaSetDirectoryInfo.GetFiles("*", SearchOption.AllDirectories)
+                .Where(file => _mediaSetOrganizerSettings.MediaSet.SupportedVideoExtensions.Contains(file.Extension.ToLower()))
+                .ToList();
+
+            // Der Titel des Mediensets ist der Verzeichnisname
+            var mediaSetTitle = mediaSetDirectoryInfo.Name;
+
+            _logger.LogInformation("Im Medienset {mediaSetTitle} wurden {supportedMediaByMediaSet.Count} unterstützte Medien gefunden", mediaSetTitle, supportedMediaByMediaSet.Count);
+        }
 
         var infuseXmlFiles = new List<FileInfo>();
         return Task.FromResult(Result.Success(infuseXmlFiles));
