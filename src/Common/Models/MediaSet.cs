437492b0f7c5ad0f73005c4e47a996f57c035f1b
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-07-10 11:24:30 +0200

    Added useful image properties for media set type

--- src/Common/Models/MediaSet.cs
+++ src/Common/Models/MediaSet.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using CSharpFunctionalExtensions;
 using Kurmann.Videoschnitt.Common.Entities.MediaTypes;
 
@@ -20,5 +21,19 @@ public record MediaSet
     public Maybe<List<SupportedImage>> ImageFiles { get; set; }
 
     public Maybe<Masterfile> Masterfile { get; set; }
+
+    public bool IsNoImageFiles => ImageFiles.HasNoValue || ImageFiles.Value.Count == 0;
+    public bool IsSingleImageFile => ImageFiles.HasValue && ImageFiles.Value.Count == 1;
+    public bool IsTwoImageFiles => ImageFiles.HasValue && ImageFiles.Value.Count == 2;
+    public bool IsMoreThanTwoImageFiles => ImageFiles.HasValue && ImageFiles.Value.Count > 2;
+
+    /// <summary>
+    /// Gibt die zwei Bilder zurück mit dem jüngsten Änderungsdatum.
+    /// </summary>
+    public (SupportedImage image1, SupportedImage image2) GetTwoLatestImages()
+    {
+        var images = ImageFiles.Value.OrderByDescending(x => x.FileInfo.LastWriteTime).Take(2).ToList();
+        return (images[0], images[1]);
+    }
 }
 
