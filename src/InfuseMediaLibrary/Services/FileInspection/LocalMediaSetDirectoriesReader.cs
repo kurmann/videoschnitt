9a481bb42694c73fb6c469db34b7b05ae504df16
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-07-13 19:01:31 +0200

    implemented local media set directory reader

---
+++ src/InfuseMediaLibrary/Services/FileInspection/LocalMediaSetDirectoriesReader.cs
@@ -0,0 +1,91 @@
+using CSharpFunctionalExtensions;
+using Kurmann.Videoschnitt.ConfigurationModule.Settings;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Kurmann.Videoschnitt.InfuseMediaLibrary.Services.FileInspection;
+
+/// <summary>
+/// Verantwortlich für das Auslesen der Verzeichnisse, in denen die Mediensets gespeichert sind.
+/// </summary>
+internal class LocalMediaSetDirectoryReader
+{
+    private readonly ILogger<LocalMediaSetDirectoryReader> _logger;
+    private readonly MediaSetOrganizerSettings _mediaSetOrganizerSettings;
+
+    public LocalMediaSetDirectoryReader(ILogger<LocalMediaSetDirectoryReader> logger, IOptions<MediaSetOrganizerSettings> mediaSetOrganizerSettings)
+    {
+        _logger = logger;
+        _mediaSetOrganizerSettings = mediaSetOrganizerSettings.Value;
+    }
+
+    public Result<List<LocalMediaSetDirectory>> GetMediaSetDirectories(string directoryPath)
+    {
+        try
+        {
+            var directoryInfo = new DirectoryInfo(directoryPath);
+            return GetMediaSetDirectories(directoryInfo);
+        }
+        catch
+        {
+            return Result.Failure<List<LocalMediaSetDirectory>>($"Das Verzeichnis '{directoryPath}' konnte nicht geöffnet werden.");
+        }
+    }
+
+    public Result<List<LocalMediaSetDirectory>> GetMediaSetDirectories(DirectoryInfo directoryInfo)
+    {
+        try
+        {
+            // Prüfen, ob das Verzeichnis existiert
+            if (!directoryInfo.Exists)
+            {
+                return Result.Failure<List<LocalMediaSetDirectory>>($"Das Verzeichnis '{directoryInfo.FullName}' existiert nicht.");
+            }
+
+            // Ermittle alle Unterverzeichnisse des Quellverzeichnisses
+            var mediaSetDirectories = directoryInfo.GetDirectories();
+            if (mediaSetDirectories.Length == 0)
+            {
+                _logger.LogInformation("Keine Mediensets im Verzeichnis {Directory} gefunden.", directoryInfo.FullName);
+                return Result.Success(new List<LocalMediaSetDirectory>());
+            }
+
+            var localMediaSetDirectories = new List<LocalMediaSetDirectory>();
+            foreach (var mediaSetDirectory in mediaSetDirectories)
+            {
+                var mediaSetName = mediaSetDirectory.Name;
+
+                // Suche nach dem Unterverzeichnis, das die Dateien für den Medienserver enthält
+                var mediaServerFilesDirectory = mediaSetDirectory.GetDirectories()
+                    .FirstOrDefault(d => d.Name == _mediaSetOrganizerSettings.MediaSet.MediaServerFilesSubDirectoryName);
+
+                // Suche nach dem Unterverzeichnis, das die Artwork-Bilder für das Medienset enthält
+                var artworkDirectory = mediaSetDirectory.GetDirectories()
+                    .FirstOrDefault(d => d.Name == _mediaSetOrganizerSettings.MediaSet.ImageFilesSubDirectoryName);
+
+                var localMediaSetDirectory = new LocalMediaSetDirectory(mediaSetDirectory, mediaSetName, artworkDirectory ?? Maybe<DirectoryInfo>.None, mediaServerFilesDirectory ?? Maybe<DirectoryInfo>.None);
+                localMediaSetDirectories.Add(localMediaSetDirectory);
+            }
+
+            return Result.Success(localMediaSetDirectories);
+        }
+        catch
+        {
+            return Result.Failure<List<LocalMediaSetDirectory>>($"Das Verzeichnis '{directoryInfo.FullName}' konnte nicht geöffnet werden.");
+        }
+    
+    }
+}
+
+internal record LocalMediaSetDirectory(DirectoryInfo DirectoryInfo, string MediaSetName, Maybe<DirectoryInfo> ArtworkDirectory, Maybe<DirectoryInfo> MediaServerFilesDirectory)
+{
+    public override string ToString()
+    {
+        return MediaSetName;
+    }
+
+    public static implicit operator DirectoryInfo(LocalMediaSetDirectory localMediaSetDirectory)
+    {
+        return localMediaSetDirectory.DirectoryInfo;
+    }
+}
