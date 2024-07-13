85b1a0f13126665b23dce22e74d4dfc8e9983174
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-07-13 06:56:54 +0200

    Introduced artwork image integrator

---
+++ src/InfuseMediaLibrary/Services/Integration/ArtworkImageIntegrator.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kurmann.Videoschnitt.InfuseMediaLibrary.Services.Integration;
+
+/// <summary>
+/// Verwantwortlich für die Integration von Bildern in die lokale Infuse-Mediathek für die Anzeige als Titelbilder auf dem Infuse-Medienserver.
+/// </summary>
+public class ArtworkImageIntegrator
+{
+    
+}
