28e644cf794fb9ce9fe33cdbe4d4c239355a223c
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-08-02 20:11:59 +0200

    feat: Add DirectoryCleanupService to handle cleanup of unnecessary files and directories

--- src/InfuseMediaLibrary/ServiceCollectionExtensions.cs
+++ src/InfuseMediaLibrary/ServiceCollectionExtensions.cs
@@ -22,6 +22,7 @@ public static class ServiceCollectionExtensions
         services.AddScoped<MediaSetIntegrator>();
         services.AddScoped<MetadataFileIntegrator>();
         services.AddScoped<InfuseMediaIntegrator>();
+        services.AddScoped<DirectoryCleanupService>();
 
         return services;
     }
