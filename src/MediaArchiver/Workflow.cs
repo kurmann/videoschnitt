279af05bb4da8361c02589a4e70ccec60147d90a
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-08-03 19:11:21 +0200

    feat: Add Workflow class for archiving media

---
+++ src/MediaArchiver/Workflow.cs
@@ -0,0 +1,35 @@
+using System;
+using Microsoft.Extensions.Logging;
+using CSharpFunctionalExtensions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Kurmann.Videoschnitt.MediaArchiver;
+
+public interface IWorkflow
+{
+    const string WorkflowName = "MediaArchiver";
+
+    Task<Result> ExecuteAsync();
+}
+
+/// <summary>
+/// Archiviert Medien, vorwiegend Mediensets, in vorbestimmte Verzeichnisse für die Langzeitarchivierung.
+/// </summary>
+internal class Workflow : IWorkflow
+{
+    private readonly ILogger<Workflow> _logger;
+
+    public Workflow(ILogger<Workflow> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<Result> ExecuteAsync()
+    {
+        _logger.LogInformation("Starting MediaArchiver workflow.");
+        return Result.Success();
+    }
+}
