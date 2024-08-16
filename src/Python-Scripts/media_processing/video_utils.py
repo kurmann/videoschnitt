5138be41414ce33e777f272188fb08dba6eac894
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-08-16 09:08:53 +0200

    Alles über 80 mbit HEVC wird als HEVC-A interpretiert

--- src/Python-Scripts/media_processing/video_utils.py
+++ src/Python-Scripts/media_processing/video_utils.py
@@ -18,6 +18,22 @@ def get_video_codec(filepath):
     else:
         return None
 
+def get_bitrate(filepath):
+    """
+    Führt ffprobe aus, um die Bitrate der Videodatei zu ermitteln.
+    """
+    cmd = ['ffprobe', '-v', 'error', '-select_streams', 'v:0', '-show_entries', 'stream=bit_rate', '-of', 'json', filepath]
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    
+    # JSON-Antwort parsen
+    probe = json.loads(result.stdout)
+    
+    # Überprüfen, ob der Videostream vorhanden ist und die Bitrate extrahieren
+    if 'streams' in probe and len(probe['streams']) > 0 and 'bit_rate' in probe['streams'][0]:
+        return int(probe['streams'][0]['bit_rate'])
+    else:
+        return None
+
 def file_in_use(filepath):
     """
     Prüft, ob eine Datei in Verwendung ist, indem versucht wird, sie umzubenennen.
