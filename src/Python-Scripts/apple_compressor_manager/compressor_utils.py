d09e0205c564998ffb6ecdf8abb63230777422e3
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-08-17 15:13:15 +0200

    Logik in zwei Python-Packete aufgeteilt: `Apple Compressor Manager` und `Original Media Organizer` (#502)
    * removed compression logic from integration logic
    
    * Apple Compressor Manager und Original Media Organizer als zwei Python-Packages definiert

---
+++ src/Python-Scripts/apple_compressor_manager/compressor_utils.py
@@ -0,0 +1,16 @@
+import os
+        
+def are_sb_files_present(hevc_a_path):
+    """
+    Überprüft, ob temporäre .sb-Dateien vorhanden sind, die auf eine laufende Komprimierung hinweisen.
+    Diese Prüfung erfolgt auf das Vorkommen von ".sb-" im Dateinamen.
+    """
+    directory = os.path.dirname(hevc_a_path)
+    base_name = os.path.basename(hevc_a_path).replace('.mov', '')
+
+    for filename in os.listdir(directory):
+        if base_name in filename and ".sb-" in filename:
+            print(f"Temporäre Datei gefunden: {filename}. Warte auf Abschluss der Komprimierung.")
+            return True
+
+    return False
---
+++ src/Python-Scripts/apple_compressor_manager/file_utils.py
@@ -0,0 +1,10 @@
+import os
+import shutil
+
+def is_file_in_use(filepath):
+    """Überprüft, ob eine Datei in Verwendung ist."""
+    try:
+        os.rename(filepath, filepath)  # Versuch, die Datei umzubenennen, um sicherzustellen, dass sie nicht in Verwendung ist.
+        return False
+    except OSError:
+        return True
