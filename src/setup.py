1e1b147863b1d9cfbefe1678e45a8f2e4b321cb8
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-08-24 13:22:15 +0200

    chore: Add setup.py file for package configuration

---
+++ src/setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(__file__), '..', 'README.md'), 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name="kurmann_videoschnitt",
+    version="0.40.0",
+    description="Video editing automation tools for Kurmann Videoschnitt",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Patrick Kurmann",
+    author_email="patrick.kurmann@example.com",
+    url="https://github.com/kurmann/videoschnitt",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'import-new-media=new_media_importer.import_new_media:main',
+            'integrate-new-media=original_media_integrator.integrate_new_media:main',
+            'compress-prores=apple_compressor_manager.compress_prores:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
