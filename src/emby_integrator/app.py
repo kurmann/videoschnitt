6d0721c510752f696014e2278d75e05545000664
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-09-09 22:18:38 +0200

    refactoring some methods to image_manager from file_manager

--- src/emby_integrator/app.py
+++ src/emby_integrator/app.py
@@ -2,10 +2,12 @@ import json
 import os
 import typer
 from emby_integrator.file_manager import FileManager
+from emby_integrator.image_manager import ImageManager
 
 
 # Erstelle eine Instanz von FileManager
 file_manager = FileManager()
+image_manager = ImageManager()
 
 # Erstelle die Typer-App
 app = typer.Typer(help="FileManager CLI für Emby Integrator")
@@ -86,7 +88,7 @@ def convert_image_to_adobe_rgb(image_file: str):
     """Erstelle Adobe RGB-JPG-Datei"""
     # Ausgabedatei auf Basis des Eingabedateinamens (im selben Verzeichnis mit .jpg)
     output_file = f"{os.path.splitext(image_file)[0]}.jpg"
-    file_manager.convert_image_to_adobe_rgb(image_file, output_file)
+    image_manager.convert_image_to_adobe_rgb(image_file, output_file)
 
 @app.command()
 def convert_images_to_adobe_rgb(media_dir: str):
@@ -95,12 +97,12 @@ def convert_images_to_adobe_rgb(media_dir: str):
     :param media_dir: Verzeichnis, das sowohl die PNG-Bilder als auch die Videodateien enthält
     """
     image_files = [os.path.join(media_dir, f) for f in os.listdir(media_dir) if f.lower().endswith(".png")]
-    file_manager.convert_images_to_adobe_rgb(image_files, media_dir)
+    image_manager.convert_images_to_adobe_rgb(image_files, media_dir)
 
 @app.command()
 def get_images_for_artwork(directory: str):
     """Rufe geeignete Bilder für Artwork aus einem Verzeichnis ab."""
-    file_manager.get_images_for_artwork(directory)
+    image_manager.get_images_for_artwork(directory)
 
 if __name__ == '__main__':
     app()
