f54c75fda763e7cb89e021a5d2b4b7dfdfceff9e
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-09-12 22:12:33 +0200

    Entferne die komplette Legacy .NET-Anwendung aus dem Quelltext (#657)

--- src/Common/Models/Masterfile.cs
+++
@@ -1,7 +0,0 @@
-namespace Kurmann.Videoschnitt.Common.Models;
-
-/// <summary>
-/// Repräsentiert eine Masterdatei.
-/// Sie ist die Videodatei in der höchsten Qualität und als Ausgangslage für alle weiteren Prozesse.
-/// </summary>
-public record Masterfile(FileInfo FileInfo, string Codec, string? Profile);
