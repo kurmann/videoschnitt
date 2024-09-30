61b6eaf76d5e4cfcfb1057116c659e9176cec3d3
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-09-30 20:16:10 +0200

    API-Methoden entfernt, die Abhängigkeiten mit Apple Compressor Job Manager hatten

--- src/emby_integrator/app.py
+++ src/emby_integrator/app.py
@@ -10,82 +10,12 @@ import json
 import os
 import typer
 import xml.etree.ElementTree as ET
-from emby_integrator.mediaset_manager import get_mediaserver_files
 from metadata_manager import get_relevant_metadata, parse_recording_date
 from emby_integrator.nfo_generator import CustomProductionInfuseMetadata
-from emby_integrator.video_manager import compress_masterfile
 from emby_integrator.image_manager import convert_images_to_adobe_rgb
 from emby_integrator.xml_utils import indent
 
 app = typer.Typer(help="Emby Integrator")
-
-@app.command()
-def list_mediaserver_files(
-    source_dir: str, 
-    json_output: bool = typer.Option(False, help="Gebe die Ausgabe im JSON-Format aus")
-):
-    """
-    Liste die Mediaserver-Dateien aus einem Verzeichnis auf und gruppiere sie nach Mediensets.
-
-    Diese Methode durchsucht das angegebene Verzeichnis nach Videodateien und zugehörigen Titelbildern 
-    und gruppiert diese nach Mediensets. Falls das Flag `--json-output` gesetzt wird, wird die Ausgabe 
-    im JSON-Format zurückgegeben, andernfalls wird eine menschenlesbare Ausgabe erstellt, die die 
-    Informationen bündig darstellt.
-
-    Args:
-        source_dir (str): Der Pfad zu dem Verzeichnis, das nach Mediendateien und Bildern durchsucht wird.
-        json_output (bool): Optional. Wenn gesetzt, wird die Ausgabe im JSON-Format dargestellt. Standard ist `False`.
-
-    Returns:
-        None: Gibt die Mediensets in einer menschenlesbaren Form oder als JSON zurück, je nach dem Wert von `json_output`.
-    
-    Raises:
-        FileNotFoundError: Wenn das angegebene Verzeichnis nicht existiert.
-    """
-    media_sets = get_mediaserver_files(source_dir)
-    
-    if json_output:
-        # JSON-Ausgabe
-        print(json.dumps(media_sets, indent=4))
-    else:
-        # Bündige Ausgabe der Informationen
-        max_label_length = 10  # Feste Länge für die Labels "Videos" und "Titelbild"
-        
-        for set_name, data in media_sets.items():
-            print(f"Medienset: {set_name}")
-            print(f"{'Videos:':<{max_label_length}} {', '.join(data['videos']) if data['videos'] else 'Keine Videodateien gefunden.'}")
-            print(f"{'Titelbild:':<{max_label_length}} {data['image'] if data['image'] else 'Kein Titelbild gefunden.'}")
-            print("-" * 40)
-
-
-@app.command(name="compress-masterfile")
-def compress_masterfile_command(
-    input_file: str, 
-    delete_master_file: bool = typer.Option(False, help="Lösche die Master-Datei nach der Komprimierung.")
-):
-    """
-    Komprimiere eine Master-Datei.
-
-    Diese Methode startet die Kompression der angegebenen Datei und bietet die Möglichkeit, 
-    nach Abschluss der Komprimierung die Originaldatei zu löschen.
-
-    Args:
-        input_file (str): Der Pfad zur Master-Datei, die komprimiert werden soll.
-        delete_master_file (bool): Optional. Gibt an, ob die Master-Datei nach der Komprimierung gelöscht werden soll. Standard ist `False`.
-
-    Returns:
-        None
-    
-    Beispiel:
-        $ emby-integrator compress-masterfile /path/to/masterfile.mov --delete-master-file
-    """
-    
-    # Definiere einen Callback, der eine Benachrichtigung sendet, wenn die Komprimierung abgeschlossen ist
-    def notify_completion(input_file, output_file):
-        print(f"Komprimierung abgeschlossen für: {input_file}")
-    
-    compress_masterfile(input_file, delete_master_file, callback=notify_completion)
-
 
 @app.command(name="convert-images-to-adobe-rgb")
 def convert_images_to_adobe_rgb_command(media_dir: str):
