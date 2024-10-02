b8884ba9c5b1e65a8ed482da6ec9f8d1c0a7fc43
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-10-02 16:51:19 +0200

    Emby Integrator: Commands anstelle einer grossen `app.py` (Achitekturverbesserung)

---
+++ src/emby_integrator/commands/__init__.py
@@ -0,0 +1,16 @@
+# src/emby_integrator/commands/__init__.py
+
+import typer
+
+def register_commands(app: typer.Typer):
+    from .convert_images import register as register_convert_images
+    from .list_metadata import register as register_list_metadata
+    from .generate_nfo_xml import register as register_generate_nfo_xml
+    from .write_nfo_file import register as register_write_nfo_file
+    from .scan_media import register as register_scan_media
+
+    register_convert_images(app)
+    register_list_metadata(app)
+    register_generate_nfo_xml(app)
+    register_write_nfo_file(app)
+    register_scan_media(app)
