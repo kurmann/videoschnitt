57f9936eeea0ebb2b410f81ec43a5a82c4f8e4ee
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-10-10 15:14:44 +0200

    refactor styles and css to dedicated methods

--- src/online_medialibrary_manager/html_generator.py
+++ src/online_medialibrary_manager/html_generator.py
@@ -11,6 +11,110 @@ from datetime import datetime
 from metadata_manager import get_metadata_with_exiftool, parse_recording_date
 from online_medialibrary_manager.commands.create_og_image import create_og_image
 
+def generate_css() -> str:
+    """
+    Generiert das CSS für die HTML-Seite.
+    
+    Returns:
+        str: Der CSS-String.
+    """
+    return '''
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: -apple-system, BlinkMacSystemFont, 'Helvetica Neue', Helvetica, Arial, sans-serif;
+            color: #e0e0e0;
+            background-color: #181818;
+        }
+
+        h1 {
+            font-size: 1.8em;
+            color: #ffffff;
+            text-align: center;
+            font-weight: 400;
+            padding-bottom: 0.7rem;
+            border-bottom: 1px solid silver;
+            letter-spacing: 0.1em;
+        }
+
+        .container {
+            max-width: 900px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #1e1e1e;
+            border-radius: 10px;
+            box-shadow: 0 8px 20px rgba(0, 0, 0, 0.5);
+        }
+
+        .video-image {
+            width: 100%;
+            height: auto;
+            border-radius: 8px;
+        }
+
+        footer {
+            text-align: center;
+            padding: 20px;
+            color: #777;
+            font-size: 0.9em;
+        }
+    </style>
+    '''
+
+def generate_javascript(high_res_file_name: str, mid_res_file_name: str) -> str:
+    """
+    Generiert das JavaScript für die HTML-Seite.
+    
+    Args:
+        high_res_file_name (str): Der Name der 4K-Datei.
+        mid_res_file_name (str): Der Name der HD-Datei.
+    
+    Returns:
+        str: Der JavaScript-String.
+    """
+    return f'''
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {{
+            var playLink = document.getElementById('play-link');
+            var highResFile = '{high_res_file_name}';
+            var midResFile = '{mid_res_file_name}';
+
+            function canPlayHEVC() {{
+                var video = document.createElement('video');
+                return video.canPlayType('video/mp4; codecs="hvc1"') !== '';
+            }}
+
+            function getQueryParam(param) {{
+                var urlParams = new URLSearchParams(window.location.search);
+                return urlParams.get(param);
+            }}
+
+            function getAnchorParam() {{
+                return window.location.hash.substr(1);
+            }}
+
+            var playParam = getQueryParam('play') || getAnchorParam();
+
+            if (playParam) {{
+                if (playParam.toLowerCase() === '4k') {{
+                    playLink.href = highResFile;
+                }} else if (playParam.toLowerCase() === 'hd') {{
+                    playLink.href = mid_res_file_name;
+                }} else {{
+                    playLink.href = midRes_file_name;
+                }}
+            }} else {{
+                if (canPlayHEVC()) {{
+                    playLink.href = highResFile;
+                }} else {{
+                    playLink.href = mid_res_file_name;
+                }}
+            }}
+        }});
+    </script>
+    '''
+    
 def generate_html(metadata_source: str, high_res_file: str, mid_res_file: str, artwork_image: str, download_file: str = None, base_url: str = '') -> str:
     """
     Generiert den HTML-Inhalt basierend auf den bereitgestellten Dateien und Metadaten.
@@ -35,7 +139,6 @@ def generate_html(metadata_source: str, high_res_file: str, mid_res_file: str, a
         try:
             locale.setlocale(locale.LC_TIME, 'de_DE.UTF-8')
         except locale.Error:
-            # Fallback, falls die deutschen Locale-Einstellungen nicht verfügbar sind
             locale.setlocale(locale.LC_TIME, '')
 
     # Extrahieren der Metadaten aus der angegebenen Metadatenquelle
@@ -88,146 +191,10 @@ def generate_html(metadata_source: str, high_res_file: str, mid_res_file: str, a
     <!-- OpenGraph Metadaten -->
     {og_meta_tags}
 
-    <style>
-        /* Modernisiertes CSS für eine ansprechendere Darstellung */
-        
-        body {{
-            margin: 0;
-            padding: 0;
-            font-family: -apple-system, BlinkMacSystemFont, 'Helvetica Neue', Helvetica, Arial, sans-serif;
-            color: #e0e0e0;
-            background-color: #181818;
-        }}
+    <!-- CSS -->
+    {generate_css()}
 
-        h1 {{
-            font-size: 1.8em;
-            color: #ffffff;
-            text-align: center;
-            font-weight: 400;
-            padding-bottom: 0.7rem;
-            border-bottom: 1px solid silver;
-            letter-spacing: 0.1em;
-        }}
-
-        h2 {{
-            color: #ffffff;
-            text-align: center;
-            font-weight: 300;
-            letter-spacing: 0.1em;
-        }}
-
-        h2.subtitle {{
-            font-size: 1em;
-        }}
-
-        h2.title-link {{
-            font-size: 1.5em;
-        }}
-
-        .container {{
-            max-width: 900px;
-            margin: 0 auto;
-            padding: 20px;
-            background-color: #1e1e1e;
-            border-radius: 10px;
-            box-shadow: 0 8px 20px rgba(0, 0, 0, 0.5);
-        }}
-
-        .video-container {{
-            position: relative;
-            display: inline-block;
-            overflow: hidden;
-            cursor: pointer;
-            border-radius: 8px;
-        }}
-
-        .video-image {{
-            width: 100%;
-            height: auto;
-            display: block;
-            border-radius: 8px;
-            transition: transform 0.3s ease-in-out;
-        }}
-
-        .video-container:hover .video-image {{
-            transform: scale(1.05);
-        }}
-
-        .play-icon {{
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            width: 64px;
-            height: 64px;
-            opacity: 0.9;
-            transition: opacity 0.3s ease-in-out;
-        }}
-
-        .video-container:hover .play-icon {{
-            opacity: 1;
-        }}
-
-        .video-details {{
-            position: absolute;
-            bottom: 15px;
-            left: 15px;
-            color: #ffffff;
-            background-color: rgba(0, 0, 0, 0.6);
-            padding: 10px 15px;
-            font-size: 1em;
-            border-radius: 5px;
-        }}
-
-        .video-description {{
-            margin: 20px;
-            color: #b0b0b0;
-            font-size: 1.1em;
-            line-height: 1.7em;
-            text-align: center;
-        }}
-
-        .links {{
-            text-align: center;
-            display: flex;
-            justify-content: center;
-            flex-wrap: wrap;
-            gap: 20px;
-            margin-top: 30px;
-        }}
-
-        .links a {{
-            color: #ffae42;
-            text-decoration: none;
-            padding: 10px 15px;
-            border: 1px solid #ffae42;
-            border-radius: 5px;
-            transition: background-color 0.3s ease, color 0.3s ease;
-            width: 200px;
-            text-align: center;
-        }}
-
-        .links span {{
-            color: #b0b0b0;
-            font-size: 0.8em;
-            margin-top: 5px;
-            display: block;
-        }}
-
-        .links a:hover {{
-            background-color: #ffae42;
-            color: #181818;
-        }}
-
-        footer {{
-            text-align: center;
-            padding: 20px;
-            color: #777;
-            font-size: 0.9em;
-        }}
-    </style>
 </head>
-
 <body>
     <h1>Kurmann Mediathek</h1>
     <h2 class="subtitle">Familienfilm-Freigabe von Patrick Kurmann</h2>
@@ -236,8 +203,7 @@ def generate_html(metadata_source: str, high_res_file: str, mid_res_file: str, a
 
         <div class="video-container" id="video-container">
             <a href="{mid_res_file_name}" id="play-link">
-                <img src="{artwork_image_name}" alt="{title}" class="video-image">
-                <!-- Play-Icon -->
+                <img src="{artwork_image}" alt="{title}" class="video-image">
                 <div class="play-icon">
                     <svg width="100%" height="100%" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" fill="#ffffff">
                         <circle cx="32" cy="32" r="32" opacity="0.7"/>
@@ -272,46 +238,8 @@ def generate_html(metadata_source: str, high_res_file: str, mid_res_file: str, a
         &copy; {datetime.now().year} Kurmann Online-Mediathek von Patrick Kurmann. Alle Rechte vorbehalten.
     </footer>
 
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {{
-            var playLink = document.getElementById('play-link');
-            var highResFile = '{high_res_file_name}';
-            var midResFile = '{mid_res_file_name}';
-
-            function canPlayHEVC() {{
-                var video = document.createElement('video');
-                return video.canPlayType('video/mp4; codecs="hvc1"') !== '';
-            }}
-
-            function getQueryParam(param) {{
-                var urlParams = new URLSearchParams(window.location.search);
-                return urlParams.get(param);
-            }}
-
-            function getAnchorParam() {{
-                return window.location.hash.substr(1); // Entfernt das '#'
-            }}
-
-            var playParam = getQueryParam('play') || getAnchorParam();
-
-            if (playParam) {{
-                if (playParam.toLowerCase() === '4k') {{
-                    playLink.href = highResFile;
-                }} else if (playParam.toLowerCase() === 'hd') {{
-                    playLink.href = midResFile;
-                }} else {{
-                    // Standard
-                    playLink.href = midResFile;
-                }}
-            }} else {{
-                if (canPlayHEVC()) {{
-                    playLink.href = highResFile;
-                }} else {{
-                    playLink.href = midResFile;
-                }}
-            }}
-        }});
-    </script>
+    <!-- JavaScript -->
+    {generate_javascript(high_res_file_name, mid_res_file_name)}
 </body>
 
 </html>
