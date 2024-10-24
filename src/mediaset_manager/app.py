089fcc07fc55fbbec387a86032be38c63a3ff699
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-10-24 15:49:25 +0200

    create mediaset by single file

--- src/mediaset_manager/app.py
+++ src/mediaset_manager/app.py
@@ -4,7 +4,6 @@ import typer
 import logging
 
 from mediaset_manager.commands.list_mediasets import list_mediasets_command
-from mediaset_manager.commands.create_homemovie_metadata_file import create_homemovie_metadata_file
 from mediaset_manager.commands.create_homemovie import create_homemovie
 from mediaset_manager.commands.validate_metadata import validate_metadata
 from mediaset_manager.commands.validate_directory import validate_directory
@@ -17,7 +16,6 @@ app = typer.Typer()
 
 # Registriere die verbleibenden Commands direkt mit benannten Commands
 app.command("list-mediasets")(list_mediasets_command)
-app.command("create-homemovie-metadata-file")(create_homemovie_metadata_file)
 app.command("create-homemovie")(create_homemovie)
 app.command("validate-metadata")(validate_metadata)
 app.command("validate-directory")(validate_directory)
